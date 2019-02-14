96b79f42fb7b392e8b404ee82175e31a6c786d9a
LSViana <lv201122@gmail.com>
2019-02-14 15:41:56 -0200

    Adding variables file and rules for variables in PHP

---
+++ AW2/intro-php/variables.php
@@ -0,0 +1,25 @@
+<?php
+    /*
+        Rules for variables in PHP
+
+        1. Must start with $
+        2. After $ must be there letter [a-zA-Z] or underscore [_]
+        3. Can't be there a number after $
+        4. Only alphanumeric characters in the name
+        5. Variable names are case sensitive
+    */
+
+    // Declaring variables in PHP (they act as containers for storing data)
+    $text = "Lucas Viana";
+    $x = "3";
+    $y = 4;
+    $z = 5;
+
+    // Operating numbers and numbers
+    echo ($y + $z); // 4 + 5 = 9
+
+    echo ' @ ';
+
+    // Operating text and numbers
+    echo ($x + $y); // "3" + 4 = 7
+?>
